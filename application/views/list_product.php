33517a12ad148acb7dce99c4069448acd620d9ba
bthakur <bthakur@53cad62b-6926-41e7-91f1-72a61ceabebc>
2010-07-17 19:02:37 +0000

    Missed out files (newly added) in last check in

---
+++ application/views/list_product.php
@@ -0,0 +1,48 @@
+<?php $this->load->view('includes/paging'); ?>
+
+
+
+<?php
+if (count($PRODUCTS) > 0) {
+?>
+    <table cellpadding="3" cellspacing="0" border="0" id="tbllist" width="98%">
+        <tr>
+            <!--th>Id</th-->
+            <th>Name</th>
+            <th>Type</th>
+            <th>Company</th>
+            <th>Manufacturer</th>
+            <!--th>Ingredient</th-->
+            <th>Brand</th>
+            <!--th>UPC</th-->
+            <!--th>Status</th-->
+            <!--th>Fructose</th-->
+
+        </tr>
+
+
+    <?php
+    $i = 0;
+    foreach ($PRODUCTS as $r) :
+        $i++;
+        echo '<tr class="d' . ($i & 1) . '">';
+        //echo '<td>' . $r->productId . '</td>';
+        echo '<td>' . $r->productName . '</td>';
+        echo '<td>' . $r->productType . '</td>';
+        echo '<td>' . $r->companyName . '</td>';
+        echo '<td>' . $r->manufactureName . '</td>';
+        //echo '<td>' . $r->ingredient . '</td>';
+        echo '<td>' . $r->brand . '</td>';
+        //echo '<td>' . $r->upc . '</td>';
+        //echo '<td>' . $r->status . '</td>';
+        //echo '<td>' . $r->hasFructose . '</td>';
+        echo '</tr>';
+    endforeach;
+    ?>
+
+    <?php
+} else {
+    echo "No product available";
+}
+    ?>
+</table>
