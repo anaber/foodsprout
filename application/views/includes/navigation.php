f4652a26d8f1ab7e68e4b9103e5ec140584faebe
onemedia <anaber@gmail.com>
2010-07-30 09:00:19 +0000

    Implementing the new design

--- application/views/includes/navigation.php
+++ application/views/includes/navigation.php
@@ -1,12 +1,34 @@
-<div class="nav-wide"><br>
-		<div id="main-nav">
-			<ul id="navlist">		
-				<li id="active"><?php echo anchor('restaurant', 'Restaurants', 'id="current"'); ?></li>
-				<?php /*?><li><?php echo anchor('product', 'Products'); ?></li><?php */  ?>
-				<li><?php echo anchor('manufacture', 'Manufactures'); ?></li>
-				<li><?php echo anchor('distributor', 'Distributors'); ?></li>
-				<li><?php echo anchor('farm', 'Farms'); ?></li>
-				<?php /*?><li><?php echo anchor('foodweb', 'Food Web'); ?></li><?php */  ?>
-			</ul>
-		</div>
+<?php
+$tab = $this->uri->segment(1);
+?>
+
+<div id="navigation">
+	<?php
+	 	$restclass=NULL;
+		if($tab == "chain" || $tab == "restaurant") { 
+			$restclass = 'class="tabon"'; 
+		} 
+		echo anchor('restaurant', 'Restaurants', $restclass);
+	?>
+	<?php
+	 	$manuclass=NULL;
+		if($tab == "manufacture") { 
+			$manuclass = 'class="tabon"';
+		}
+		echo anchor('manufacture', 'Products', $manuclass);
+	?>
+	<?php
+		$farmclass=NULL;
+		if($tab == "farm") { 
+			$farmclass = 'class="tabon"';
+		}
+		echo anchor('farm', 'Farms', $farmclass);
+	?>
+	<?php
+		$marketclass=NULL;
+		if($tab == "farmersmarket") { 
+			$marketclass = 'class="tabon"';
+		}
+		echo anchor('farmersmarket', 'Farmers Market', $marketclass);
+	?>
 </div>
