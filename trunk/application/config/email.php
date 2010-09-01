d5a45068befcfda0f82f962096c3fdea3c048d8d
deepak2305 <deepak2305@53cad62b-6926-41e7-91f1-72a61ceabebc>
2010-09-01 01:35:05 +0000

    Config change

--- trunk/application/config/email.php
+++ trunk/application/config/email.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'mail.emailsrvr.com';
 $config['smtp_user'] = 'no-reply@foodsprout.com';
@@ -7,13 +7,5 @@ $config['smtp_pass'] = 'realF00d';
 $config['smtp_port'] = '25';
 $config['charset'] = 'iso-8859-1';
 $config['wordwrap'] = TRUE;
-*/
 
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'mail.xcelvations.com';
-$config['smtp_user'] = 'no-reply@foodsprout.com';
-//$config['smtp_pass'] = 'realF00d';
-//$config['smtp_port'] = '25';
-$config['charset'] = 'iso-8859-1';
-$config['wordwrap'] = TRUE;
 ?>
