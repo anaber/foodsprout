0cf07e4868156e79a0cbc324adbe7adbea091957
deepak2305 <deepak2305@53cad62b-6926-41e7-91f1-72a61ceabebc>
2010-09-11 22:04:12 +0000

    Moved all JS files related to admin from /js to /js/admin to make the code more organized

--- trunk/application/views/admincp/company.php
+++ trunk/application/views/admincp/company.php
@@ -1,7 +1,7 @@
 <?php echo anchor('admincp/company/add', 'Add Company'); ?><br /><br />
 
 <script src="<?php echo base_url()?>js/jquery.colorize.js" type="text/javascript"></script>
-<script src="<?php echo base_url()?>js/company_search_admin.js" type="text/javascript"></script>
-<script src="<?php echo base_url()?>js/admin_ajax_page_common_content.js" type="text/javascript"></script>
+<script src="<?php echo base_url()?>js/admin/company_search_admin.js" type="text/javascript"></script>
+<script src="<?php echo base_url()?>js/admin/admin_ajax_page_common_content.js" type="text/javascript"></script>
 
 <?php $this->load->view('admincp/includes/ajax_paging'); ?>
