c1a46d1c4407e7d1f8d08be7c86ea7b8112747eb
deepak2305 <deepak2305@53cad62b-6926-41e7-91f1-72a61ceabebc>
2010-09-13 00:27:16 +0000

    Font size, text-decoration etc

--- trunk/application/views/dashboard/includes/menu_logout.php
+++ trunk/application/views/dashboard/includes/menu_logout.php
@@ -1,5 +1,5 @@
 <span id="signup">
 	<div id="membername">
-		<strong><?php echo $this->session->userdata('firstName'); ?></strong> | <a href="/">Food Sprout Home</a> | <strong>Dashboard</strong> | <?php echo anchor('user/settings', 'Settings'); ?> | <?php echo anchor('login/signout', 'Sign Out'); ?>
+		<strong><?php echo $this->session->userdata('firstName'); ?></strong> | <a href="/" style="font-size:13px;text-decoration:none;">Food Sprout Home</a> | <strong>Dashboard</strong> | <?php echo anchor('user/settings', 'Settings', 'style="font-size:13px;text-decoration:none;"'); ?> | <?php echo anchor('login/signout', 'Sign Out', 'style="font-size:13px;text-decoration:none;"'); ?>
 	</div>
 </span>
