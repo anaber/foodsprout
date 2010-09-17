499da0c54330e57423846d1d2555a36180a3ac6e
onemedia <onemedia@53cad62b-6926-41e7-91f1-72a61ceabebc>
2010-09-17 02:18:35 +0000

    Style changes

--- trunk/application/views/includes/name.php
+++ trunk/application/views/includes/name.php
@@ -1,4 +1,4 @@
-<div id="restaurantname"><div id="logorestaurant"><h1>
+<div id="restaurantname"><div id="logorestaurant"><h1 style="font-size:24px;">
 <?php
 $i = 0;
 foreach($BREADCRUMB as $text => $link) {
