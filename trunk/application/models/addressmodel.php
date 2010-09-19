e7126472dffd5e77d128bf7e5c36695e7472ff9e
deepak2305 <deepak2305@53cad62b-6926-41e7-91f1-72a61ceabebc>
2010-09-19 22:13:45 +0000

    http://www.sproutchain.com/admincp/ opens http://www.sproutchain.com/admincp/dashboard City auto suggest at the time of add/update addresses Address - Use city_id if its not null, instead of using city.

--- trunk/application/models/addressmodel.php
+++ trunk/application/models/addressmodel.php
@@ -2,30 +2,44 @@
 
 class AddressModel extends Model{
 	
-	function prepareAddress($address, $city, $state_id, $country_id, $zipcode) {
+	function prepareAddress($address, $city, $city_id, $state_id, $country_id, $zipcode) {
 		$CI =& get_instance();
 		
+		if ( !empty($city_id) ) {
+			$CI->load->model('CityModel','',true);
+			$objCity = $CI->CityModel->getCityFromId($city_id);
+			
+			$city = $objCity->city;
+		}
+		
 		$CI->load->model('StateModel','',true);
-		$arrState = $CI->StateModel->getStateFromId($state_id);
+		$objState = $CI->StateModel->getStateFromId($state_id);
 		
 		$CI->load->model('CountryModel','',true);
-		$arrCountry = $CI->CountryModel->getCountryFromId($country_id);
+		$objCountry = $CI->CountryModel->getCountryFromId($country_id);
 		
-		$address = $address . ' ' . $city . ' ' . $arrState->stateName . ' ' . $arrCountry->countryName . ' ' . $zipcode;
+		$address = $address . ' ' . $city . ' ' . $objState->stateName . ' ' . $objCountry->countryName . ' ' . $zipcode;
 		
 		return $address;
 	}
 	
-	function prepareAddressToDisplay($address, $city, $state_id, $country_id, $zipcode) {
+	function prepareAddressToDisplay($address, $city, $city_id, $state_id, $country_id, $zipcode) {
 		$CI =& get_instance();
 		
+		if ( !empty($city_id) ) {
+			$CI->load->model('CityModel','',true);
+			$objCity = $CI->CityModel->getCityFromId($city_id);
+			
+			$city = $objCity->city;
+		}
+		
 		$CI->load->model('StateModel','',true);
-		$arrState = $CI->StateModel->getStateFromId($state_id);
+		$objState = $CI->StateModel->getStateFromId($state_id);
 		
-		$CI->load->model('CountryModel','',true);
-		$arrCountry = $CI->CountryModel->getCountryFromId($country_id);
+		//$CI->load->model('CountryModel','',true);
+		//$objCountry = $CI->CountryModel->getCountryFromId($country_id);
 		
-		$address = $address . '<br>' . $city . ', ' . $arrState->stateName . ' ' . $zipcode; // $arrCountry->countryName;
+		$address = $address . '<br>' . $city . ', ' . $objState->stateName . ' ' . $zipcode; // $arrCountry->countryName;
 		
 		return $address;
 	}
@@ -83,8 +97,8 @@ class AddressModel extends Model{
 			$this->addressLib->longitude = $row['longitude'];
 			$this->addressLib->claimsSustainable = $row['claims_sustainable'];
 			
-			$this->addressLib->completeAddress = $this->prepareAddress($row['address'], $row['city'], $row['state_id'], $row['country_id'], $row['zipcode']);
-			$this->addressLib->displayAddress = $this->prepareAddressToDisplay($row['address'], $row['city'], $row['state_id'], $row['country_id'], $row['zipcode']);
+			$this->addressLib->completeAddress = $this->prepareAddress($row['address'], $row['city'], $row['city_id'], $row['state_id'], $row['country_id'], $row['zipcode']);
+			$this->addressLib->displayAddress = $this->prepareAddressToDisplay($row['address'], $row['city'], $row['city_id'], $row['state_id'], $row['country_id'], $row['zipcode']);
 			
 			$addresses[] = $this->addressLib;
 			unset($this->addressLib);
@@ -113,7 +127,14 @@ class AddressModel extends Model{
 	// Get all the information about one specific manufacture from an ID
 	function getAddressFromId($addressId) {
 		
-		$query = "SELECT * FROM address WHERE address_id = " . $addressId;
+		$query = "SELECT 
+				address.*, city.city as city_name
+			FROM address 
+			LEFT JOIN city 
+				ON address.city_id = city.city_id 
+			WHERE 
+				address.address_id = " . $addressId;
+				
 		log_message('debug', "AddressModel.getAddressFromId : " . $query);
 		$result = $this->db->query($query);
 		
@@ -127,7 +148,8 @@ class AddressModel extends Model{
 		$this->addressLib->stateId = $row->state_id;
 		$this->addressLib->countryId = $row->country_id;
 		$this->addressLib->zipcode = $row->zipcode;
-		
+		$this->addressLib->cityId = $row->city_id;
+		$this->addressLib->cityName = $row->city_name;
 		
 		$this->addressLib->farmId = $row->farm_id;
 		$this->addressLib->restaurantId = $row->restaurant_id;
@@ -144,13 +166,22 @@ class AddressModel extends Model{
 		$return = true;
 		$CI =& get_instance();
 		
-		$address = $this->prepareAddress($this->input->post('address'), $this->input->post('city'), $this->input->post('stateId'), $this->input->post('countryId'), $this->input->post('zipcode') );
+		$stateId = $this->input->post('stateId');
+		$cityId = $this->input->post('cityId');
+		$cityName = $this->input->post('cityName');
+		
+		if ( !empty ($cityName) && empty ($cityId)  ) {
+			$CI->load->model('CityModel','',true);
+			$cityId = $CI->CityModel->addCity($cityName, $stateId);
+		}
+		
+		$address = $this->prepareAddress($this->input->post('address'), $this->input->post('city'), $this->input->post('cityId'), $this->input->post('stateId'), $this->input->post('countryId'), $this->input->post('zipcode') );
 		
 		$CI->load->model('GoogleMapModel','',true);
 		
 		$latLng = $CI->GoogleMapModel->geoCodeAddressV3($address);
 		
-		$query = "INSERT INTO address (address_id, address, city, state_id, zipcode, country_id, latitude , longitude, ";
+		$query = "INSERT INTO address (address_id, address, city, city_id, state_id, zipcode, country_id, latitude , longitude, ";
 		if ( !empty($restaurantId) ) {
 			$query .= 'restaurant_id';
 		} else if ( !empty($farmId) ) {
@@ -163,7 +194,7 @@ class AddressModel extends Model{
 			$query .= 'farmers_market_id';
 		}
 		$query .= ", company_id, geocoded, claims_sustainable)" .
-				" values (NULL, \"" . $this->input->post('address') . "\", \"" . $this->input->post('city') . "\", '" . $this->input->post('stateId') . "', '" . $this->input->post('zipcode') . "', '" . $this->input->post('countryId') . "', '" . ( isset($latLng['latitude']) ? $latLng['latitude']:'' ) . "', '" . ( isset($latLng['longitude']) ? $latLng['longitude']:'' ) . "', ";
+				" values (NULL, \"" . $this->input->post('address') . "\", \"" . $this->input->post('city') . "\", '" . $cityId . "', '" . $stateId . "', '" . $this->input->post('zipcode') . "', '" . $this->input->post('countryId') . "', '" . ( isset($latLng['latitude']) ? $latLng['latitude']:'' ) . "', '" . ( isset($latLng['longitude']) ? $latLng['longitude']:'' ) . "', ";
 		if ( !empty($restaurantId) ) {
 			$query .= $restaurantId;
 		} else if ( !empty($farmId) ) {
@@ -255,7 +286,16 @@ class AddressModel extends Model{
 		
 		$CI =& get_instance();
 		
-		$address = $this->prepareAddress($this->input->post('address'), $this->input->post('city'), $this->input->post('stateId'), $this->input->post('countryId'), $this->input->post('zipcode') );
+		$stateId = $this->input->post('stateId');
+		$cityId = $this->input->post('cityId');
+		$cityName = $this->input->post('cityName');
+		
+		if ( !empty ($cityName) && empty ($cityId)  ) {
+			$CI->load->model('CityModel','',true);
+			$cityId = $CI->CityModel->addCity($cityName, $stateId);
+		}
+		
+		$address = $this->prepareAddress($this->input->post('address'), $this->input->post('city'), $this->input->post('cityId'), $this->input->post('stateId'), $this->input->post('countryId'), $this->input->post('zipcode') );
 				
 		$CI->load->model('GoogleMapModel','',true);
 		
@@ -264,7 +304,8 @@ class AddressModel extends Model{
 		$data = array(
 					'address' => $this->input->post('address'), 
 					'city' => $this->input->post('city'),
-					'state_id' => $this->input->post('stateId'),
+					'city_id' => $cityId,
+					'state_id' => $stateId,
 					'country_id' => $this->input->post('countryId'),
 					'zipcode' => $this->input->post('zipcode'),
 					'latitude' => ( isset($latLng['latitude']) ? $latLng['latitude']:'' ),
