5d62779356f60ea8eed3854d16d36f9a9dae69f3
deepak2305 <deepak@yuvisha.com>
2010-10-05 04:37:02 +0000

    Properties file for liver server Initial RSS file

---
+++ includes/env_properties/live_server.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Created on 19-Feb10
+ */
+ 
+/*
+ * ======================================================
+ * Application Configurations for Deepak's Box
+ * ======================================================
+ */
+ 
+/*
+ * ======================================================
+ * Database Setup
+ * ======================================================
+ */
+$DB_HOST  = "mysql50-79.wc1.dfw1.stabletransit.com";
+$DB_USER  = "492717_foodie";
+$DB_PASSWORD  = "R3alF00dRulz";
+$DATABSE  = "492717_livefood";
+
+$BASE_URL = "http://www.foodsprout.com/";
+
+$GOOGLE_MAP_KEY = 'ABQIAAAAQqYzTkvYG4V3AMPKiW-2gxRBVVCTRTpZzHjftqSGv_IhRL3B1xQDv6pu-1WNttis0X-tXtwBdD36Jg';
+
+$PER_PAGE = 20;
+$PER_PAGE_2 = 60;
+
+$GEOCODE_URL = 'http://geoc.foodsprout.com/geocode.php';
+
+/*
+ * ======================================================
+ * Table Names
+ * ======================================================
+ */
+
+/*
+ * ======================================================
+ * General Site Details
+ * ======================================================
+ */
+$SITE_TITLE  = "Food Sprout";
+
+$SYSTEM_FOLDER = "/mnt/stor2-wc1-dfw1/404946/492717/www.foodsprout.com/web/system";
+$APPLICATION_FOLDER = "/mnt/stor2-wc1-dfw1/404946/492717/www.foodsprout.com/web/application";
+
+$UPLOAD_FOLDER = "/mnt/stor2-wc1-dfw1/404946/492717/www.foodsprout.com/web/content/uploads";
+?>
