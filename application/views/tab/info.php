aa4b09a98cf633380b9e1865dc659d4bf47897e6
deepak2305 <deepak@yuvisha.com>
2010-11-26 15:47:07 +0000

    Apply CSS

--- application/views/tab/info.php
+++ application/views/tab/info.php
@@ -1,3 +1,4 @@
+<div style = "font-size: 13px;">
 Start exploring local sustainable restaurants in your area.  We'll pick up the tab.
 
 <br><br>
@@ -5,9 +6,10 @@ Current restaurants you can enter to win a gift certificate for:<br>
 <br>
 <?php
 foreach ($LOTTRIES['results'] as $lottery) {
-	echo '<a href="/restaurant/view/'.$lottery->restaurantId.'">'.$lottery->restaurantName.'</a> - '.$lottery->city.', ' . $lottery->stateCode . '  <a href="/tab/detail/'.$lottery->lotteryId.'">Enter Drawing</a><br>';
+	echo '<a href="/restaurant/view/'.$lottery->restaurantId.'" style = "font-size: 13px; text-decoration: none;">'.$lottery->restaurantName.'</a> - '.$lottery->city.', ' . $lottery->stateCode . '  <a href="/tab/detail/'.$lottery->lotteryId.'" style = "font-size: 13px; text-decoration: none;">Enter Drawing</a><br>';
 }
 ?>
+</div>
 
 
 
