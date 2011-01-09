0de699891d92c1c52d4b90163079c65e38321202
deepak2305 <deepak2305@53cad62b-6926-41e7-91f1-72a61ceabebc>
2011-01-09 21:45:10 +0000

    Left few queries by mistake in SQL file

--- trunk/database/db1 to db2 conversion.sql
+++ trunk/database/db1 to db2 conversion.sql
@@ -582,11 +582,6 @@ ALTER TABLE `custom_url` ADD `address_id` INT NOT NULL AFTER `producer_id`;
 ALTER TABLE `custom_url` ADD `city` VARCHAR( 255 ) NULL AFTER `address_id`;
 ALTER TABLE `custom_url` ADD `city_counter` INT DEFAULT NULL AFTER `city`;
 
--- ALTER TABLE `temp_custom_url` CHANGE `custom_url` `custom_url` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL ;
--- ALTER TABLE `temp_custom_url` ADD INDEX `fk_producer_slug` ( `producer_slug` ); 
--- UPDATE temp_custom_url SET custom_url = NULL;
-
-
 -- -----------------------------------------------------
 -- Delate all the old columns and data
 -- -----------------------------------------------------
