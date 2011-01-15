899ee07f0d261c666c99d1cd78633281d559193d
dantrif2001 <anaber@gmail.com>
2011-01-15 01:35:43 +0000

    custom 404 pages

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,10 +1,12 @@
 <?php 
+//$CI->load->view('includes/header.php');
+
 global $APPLICATION_FOLDER;
-require_once($APPLICATION_FOLDER.'/views/errors/header.php');
+//require_once($APPLICATION_FOLDER.'/views/includes/header.php');
 
 ?>
 		<h1><?php echo $heading; ?></h1>
 		<?php echo $message; ?>
 <?php 
-	require_once($APPLICATION_FOLDER.'/views/errors/footer.php');
+//	require_once($APPLICATION_FOLDER.'/views/includess/footer.php');
 ?>
--- application/errors/error_php.php
+++ application/errors/error_php.php
@@ -1,8 +1,5 @@
-<?php 
-global $APPLICATION_FOLDER;
-require_once($APPLICATION_FOLDER.'/views/errors/header.php');
+<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
-?>
 <h4>A PHP Error was encountered</h4>
 
 <p>Severity: <?php echo $severity; ?></p>
