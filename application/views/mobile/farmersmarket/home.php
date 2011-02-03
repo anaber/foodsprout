26049f2077251203f33829da490f56ea6ab52b4d
dantrif2001 <anaber@gmail.com>
2011-02-03 18:39:13 +0000

    farmers market mobile site

---
+++ application/views/mobile/farmersmarket/home.php
@@ -0,0 +1,7 @@
+<div id="mainarea">
+	<ul id="menu">
+		<li><a href="/mobile/farmersmarket/nearme">Sustainable Farmers Market Near Me<span>Find sustainable farmers market near you</span></a></li>
+		<li><a href="/mobile/farmersmarket/zipcode">Enter Zip Code<span>Enter Zip Code to find farmers market near you</span></a></li>
+		<li><a href="/mobile/farmersmarket/bycity">Browse by City<span>Browse Farmers Market by City</span></a></li>
+	</ul>
+</div>
--- application/views/mobile/home.php
+++ application/views/mobile/home.php
@@ -4,7 +4,6 @@
 		<li><a href="/mobile/farmersmarket">Farmers Markets <span>Find farmers markets near you</span></a></li>
 
 		<li><a href="/mobile/Farms">Farms <span>Daily Programs, Upcoming Events</span></a></li>
-		<li><a href="/mobile/tips">Tips <span>Transit, Driving Directions and Parking</span></a></li>
 		<li><a href="/mobile/fish">Fish <span>Seafood watch list</span></a></li>
 
 	</ul>
