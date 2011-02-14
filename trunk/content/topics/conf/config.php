be1ac0d2d15fc62fb553c232b4bed13c91e8384b
onemediainc <onemediainc@53cad62b-6926-41e7-91f1-72a61ceabebc>
2011-02-14 20:47:13 +0000

    Adding the base files for vanilla forum, integration will be next step

---
+++ trunk/content/topics/conf/config.php
@@ -0,0 +1,142 @@
+<?php if (!defined('APPLICATION')) exit();
+
+// Cache
+$Configuration['Cache']['Enabled'] = TRUE;
+$Configuration['Cache']['Method'] = 'dirtycache';
+$Configuration['Cache']['Filecache']['Store'] = '/Users/anaber/Sites/food/web/trunk/content/topics/cache/Filecache';
+
+// Conversations
+$Configuration['Conversations']['Version'] = '2.0.17.8';
+
+// Database
+$Configuration['Database']['Name'] = 'foodlive-new';
+$Configuration['Database']['Host'] = 'localhost';
+$Configuration['Database']['User'] = 'root';
+$Configuration['Database']['Password'] = 'root';
+$Configuration['Database']['Engine'] = 'MySQL';
+$Configuration['Database']['ConnectionOptions']['12'] = FALSE;
+$Configuration['Database']['ConnectionOptions']['1000'] = TRUE;
+$Configuration['Database']['ConnectionOptions']['1002'] = "set names 'utf8'";
+$Configuration['Database']['CharacterEncoding'] = 'utf8';
+$Configuration['Database']['DatabasePrefix'] = 'GDN_';
+$Configuration['Database']['ExtendedProperties']['Collate'] = 'utf8_unicode_ci';
+
+// EnabledApplications
+$Configuration['EnabledApplications']['Dashboard'] = 'dashboard';
+$Configuration['EnabledApplications']['Conversations'] = 'conversations';
+$Configuration['EnabledApplications']['Vanilla'] = 'vanilla';
+
+// EnabledPlugins
+$Configuration['EnabledPlugins']['GettingStarted'] = 'GettingStarted';
+$Configuration['EnabledPlugins']['HtmLawed'] = 'HtmLawed';
+
+// Garden
+$Configuration['Garden']['Title'] = 'Food Sprout';
+$Configuration['Garden']['Cookie']['Salt'] = '5V1OGT4DE1';
+$Configuration['Garden']['Cookie']['Domain'] = '';
+$Configuration['Garden']['Cookie']['Name'] = 'Vanilla';
+$Configuration['Garden']['Cookie']['Path'] = '/';
+$Configuration['Garden']['Cookie']['HashMethod'] = 'md5';
+$Configuration['Garden']['ContentType'] = 'text/html';
+$Configuration['Garden']['Charset'] = 'utf-8';
+$Configuration['Garden']['FolderBlacklist'] = array('.', '..', '_svn', '.git');
+$Configuration['Garden']['Locale'] = 'en-CA';
+$Configuration['Garden']['LocaleCodeset'] = 'UTF8';
+$Configuration['Garden']['Domain'] = '';
+$Configuration['Garden']['WebRoot'] = FALSE;
+$Configuration['Garden']['StripWebRoot'] = FALSE;
+$Configuration['Garden']['Debug'] = FALSE;
+$Configuration['Garden']['RewriteUrls'] = TRUE;
+$Configuration['Garden']['Session']['Length'] = '15 minutes';
+$Configuration['Garden']['Authenticator']['DefaultScheme'] = 'password';
+$Configuration['Garden']['Authenticator']['RegisterUrl'] = '/entry/register?Target=%2$s';
+$Configuration['Garden']['Authenticator']['SignInUrl'] = '/entry/signin?Target=%2$s';
+$Configuration['Garden']['Authenticator']['SignOutUrl'] = '/entry/signout/{Session_TransientKey}?Target=%2$s';
+$Configuration['Garden']['Authenticator']['EnabledSchemes'] = array('password');
+$Configuration['Garden']['Authenticator']['SyncScreen'] = 'smart';
+$Configuration['Garden']['Authenticators']['password']['Name'] = 'Password';
+$Configuration['Garden']['Errors']['LogEnabled'] = FALSE;
+$Configuration['Garden']['Errors']['LogFile'] = '';
+$Configuration['Garden']['Errors']['MasterView'] = 'error.master.php';
+$Configuration['Garden']['SignIn']['Popup'] = TRUE;
+$Configuration['Garden']['UserAccount']['AllowEdit'] = TRUE;
+$Configuration['Garden']['Registration']['Method'] = 'Captcha';
+$Configuration['Garden']['Registration']['DefaultRoles'] = array('8');
+$Configuration['Garden']['Registration']['ApplicantRoleID'] = 4;
+$Configuration['Garden']['Registration']['InviteExpiration'] = '-1 week';
+$Configuration['Garden']['Registration']['InviteRoles'] = FALSE;
+$Configuration['Garden']['TermsOfService'] = '/dashboard/home/termsofservice';
+$Configuration['Garden']['Email']['UseSmtp'] = FALSE;
+$Configuration['Garden']['Email']['SmtpHost'] = '';
+$Configuration['Garden']['Email']['SmtpUser'] = '';
+$Configuration['Garden']['Email']['SmtpPassword'] = '';
+$Configuration['Garden']['Email']['SmtpPort'] = '25';
+$Configuration['Garden']['Email']['SmtpSecurity'] = '';
+$Configuration['Garden']['Email']['MimeType'] = 'text/plain';
+$Configuration['Garden']['Email']['SupportName'] = 'Support';
+$Configuration['Garden']['Email']['SupportAddress'] = '';
+$Configuration['Garden']['UpdateCheckUrl'] = 'http://vanillaforums.org/addons/update';
+$Configuration['Garden']['AddonUrl'] = 'http://vanillaforums.org/addons';
+$Configuration['Garden']['CanProcessImages'] = TRUE;
+$Configuration['Garden']['Installed'] = TRUE;
+$Configuration['Garden']['Forms']['HoneypotName'] = 'hpt';
+$Configuration['Garden']['Upload']['MaxFileSize'] = '50M';
+$Configuration['Garden']['Upload']['AllowedFileExtensions'] = array('txt', 'jpg', 'gif', 'png', 'bmp', 'zip', 'gz', 'tar.gz', 'tgz', 'psd', 'ai', 'fla', 'swf', 'pdf', 'doc', 'xls', 'ppt', 'docx', 'xlsx', 'log', 'pdf');
+$Configuration['Garden']['Picture']['MaxHeight'] = 1000;
+$Configuration['Garden']['Picture']['MaxWidth'] = 600;
+$Configuration['Garden']['Profile']['MaxHeight'] = 1000;
+$Configuration['Garden']['Profile']['MaxWidth'] = 250;
+$Configuration['Garden']['Profile']['Public'] = TRUE;
+$Configuration['Garden']['Profile']['ShowAbout'] = TRUE;
+$Configuration['Garden']['Profile']['EditUsernames'] = FALSE;
+$Configuration['Garden']['Preview']['MaxHeight'] = 100;
+$Configuration['Garden']['Preview']['MaxWidth'] = 75;
+$Configuration['Garden']['Thumbnail']['Size'] = 50;
+$Configuration['Garden']['Menu']['Sort'] = array('Dashboard', 'Discussions', 'Questions', 'Activity', 'Applicants', 'Conversations', 'User');
+$Configuration['Garden']['InputFormatter'] = 'Html';
+$Configuration['Garden']['Html']['SafeStyles'] = TRUE;
+$Configuration['Garden']['Search']['Mode'] = 'matchboolean';
+$Configuration['Garden']['Theme'] = 'default';
+$Configuration['Garden']['MobileTheme'] = 'mobile';
+$Configuration['Garden']['Roles']['Manage'] = TRUE;
+$Configuration['Garden']['VanillaUrl'] = 'http://vanillaforums.org';
+$Configuration['Garden']['AllowSSL'] = TRUE;
+$Configuration['Garden']['PrivateCommunity'] = FALSE;
+$Configuration['Garden']['EditContentTimeout'] = -1;
+$Configuration['Garden']['Modules']['ShowSignedInModule'] = FALSE;
+$Configuration['Garden']['Modules']['ShowRecentUserModule'] = FALSE;
+$Configuration['Garden']['Format']['Mentions'] = TRUE;
+$Configuration['Garden']['Format']['Hashtags'] = TRUE;
+$Configuration['Garden']['Format']['YouTube'] = TRUE;
+$Configuration['Garden']['Format']['Vimeo'] = TRUE;
+$Configuration['Garden']['Format']['EmbedSize'] = 'normal';
+$Configuration['Garden']['Version'] = '2.0.17.8';
+
+// Modules
+$Configuration['Modules']['Garden']['Panel'] = array('UserPhotoModule', 'UserInfoModule', 'GuestModule', 'Ads');
+$Configuration['Modules']['Garden']['Content'] = array('MessageModule', 'Notices', 'Content', 'Ads');
+$Configuration['Modules']['Vanilla']['Panel'] = array('NewDiscussionModule', 'SignedInModule', 'GuestModule', 'Ads');
+$Configuration['Modules']['Vanilla']['Content'] = array('MessageModule', 'Notices', 'NewConversationModule', 'NewDiscussionModule', 'Content', 'Ads');
+$Configuration['Modules']['Conversations']['Panel'] = array('NewConversationModule', 'SignedInModule', 'GuestModule', 'Ads');
+$Configuration['Modules']['Conversations']['Content'] = array('MessageModule', 'Notices', 'NewConversationModule', 'NewDiscussionModule', 'Content', 'Ads');
+
+// Preferences
+$Configuration['Preferences']['Email']['ConversationMessage'] = '1';
+$Configuration['Preferences']['Email']['AddedToConversation'] = '1';
+$Configuration['Preferences']['Email']['BookmarkComment'] = '1';
+$Configuration['Preferences']['Email']['WallComment'] = '0';
+$Configuration['Preferences']['Email']['ActivityComment'] = '0';
+$Configuration['Preferences']['Email']['DiscussionComment'] = '0';
+$Configuration['Preferences']['Email']['DiscussionMention'] = '0';
+$Configuration['Preferences']['Email']['CommentMention'] = '0';
+
+// Routes
+$Configuration['Routes']['DefaultController'] = 'discussions';
+$Configuration['Routes']['Default404'] = array('dashboard/home/filenotfound', 'NotFound');
+$Configuration['Routes']['DefaultPermission'] = array('dashboard/home/permission', 'NotAuthorized');
+$Configuration['Routes']['UpdateMode'] = 'dashboard/home/updatemode';
+
+// Vanilla
+$Configuration['Vanilla']['Version'] = '2.0.17.8';
+
+// Last edited by Unknown (::1)2011-02-14 15:33:09
