55351efd1f4f4217073550dcfccbf667f14849c9
deepak2305 <deepak@yuvisha.com>
2011-02-19 07:06:45 +0000

    Make restaurant listing to work without js - url for paging and per page

--- application/models/listmodel.php
+++ application/models/listmodel.php
@@ -60,6 +60,7 @@ class ListModel extends Model{
 			'<div class = "clear"></div>' . "\n"
 			;
 		}
+		
 		return $html;
 	}
 	
@@ -75,6 +76,7 @@ class ListModel extends Model{
 		<div style="float:left; width:195px;" id = "recordsPerPage" align = "right">
 			' . $this->drawRecordsPerPage($params) . '
 		</div>';
+		$this->buildUrl($params);
 		
 		return $html;
 	}
@@ -98,40 +100,137 @@ class ListModel extends Model{
 		if ($params['perPage'] == 10) {
 			$str .= '<strong>10</strong> | ';
 		} else {
-			$str .= '<a href="#" id = "10PerPage">10</a> | ';
+			$str .= '<a href="' . $this->buildUrl($params, 'pp', '10') . '" id = "10PerPage">10</a> | ';
 		}
 		
 		if ($params['perPage'] == 20) {
 			$str .= '<strong>20</strong> | ';
 		} else {
-			$str .= '<a href="#" id = "20PerPage">20</a> | ';
+			$str .= '<a href="' . $this->buildUrl($params, 'pp', '20') . '" id = "20PerPage">20</a> | ';
 		}
 		
 		if ($params['perPage'] == 40) {
 			$str .= '<strong>40</strong> | ';
 		} else {
-			$str .= '<a href="#" id = "40PerPage">40</a> | ';
+			$str .= '<a href="' . $this->buildUrl($params, 'pp', '40') . '" id = "40PerPage">40</a> | ';
 		}
 		
 		if ($params['perPage'] == 50) {
 			$str .= '<strong>50</strong>';
 		} else {
-			$str .= '<a href="#" id = "50PerPage">50</a>';
+			$str .= '<a href="' . $this->buildUrl($params, 'pp', '50') . '" id = "50PerPage">50</a>';
 		}
 		
 		return $str;
 	}
 	
 	function drawPagingLinks($params) {
+		
+		$lastPage = floor($params['numResults']/$params['perPage']);
+		$currentPage = ($params['page'] > $lastPage ? $lastPage : $params['page']);
+		$firstPage = '0';
+		$previousPage = ( ($currentPage -1) < 0 ? 0 : ($currentPage -1));
+		$nextPage = ( ($currentPage + 1 ) >= $lastPage ? $lastPage : ($currentPage + 1) );
+		
+		$page = array (
+			'firstPage' => $firstPage,
+			'previousPage' => $previousPage,
+			'currentPage' => $currentPage,
+			'nextPage' => $nextPage,
+			'lastPage' => $lastPage
+		);
+		
 		$str = '';
-		$str .= '<a href="#" id = "imgFirst">First</a> &nbsp;&nbsp;';
-		$str .= '<a href="#" id = "imgPrevious">Previous</a> ';
-		$str .= '&nbsp;&nbsp;&nbsp; Page ' . ($params['page']+1) . ' of ' . $params['totalPages'] . '&nbsp;&nbsp;&nbsp;';
-		$str .= '<a href="#" id = "imgNext">Next</a> &nbsp;&nbsp;';
-		$str .= '<a href="#" id = "imgLast">Last</a>';
+		$str .= '<a href="' . $this->buildUrl($params, 'p', $page['firstPage']) . '" id = "imgFirst">First</a> &nbsp;&nbsp;';
+		$str .= '<a href="' . $this->buildUrl($params, 'p', $page['previousPage']) . '" id = "imgPrevious">Previous</a> ';
+		$str .= '&nbsp;&nbsp;&nbsp; Page ' . ($page['currentPage']+1) . ' of ' . $params['totalPages'] . '&nbsp;&nbsp;&nbsp;';
+		$str .= '<a href="' . $this->buildUrl($params, 'p', $page['nextPage']) . '" id = "imgNext">Next</a> &nbsp;&nbsp;';
+		$str .= '<a href="' . $this->buildUrl($params, 'p', $page['lastPage']) . '" id = "imgLast">Last</a>';
 		
 		return $str;
 	}
+	
+	function buildUrl($params, $qs = null, $value = null) {
+		$uri1 = '/' . $this->uri->segment(1);
+		$uri2 = $this->uri->segment(2);
+		$uri3 = $this->uri->segment(3);
+		$url = $uri1 . ($uri2 ? '/' . $uri2 : '') . ($uri3 ? '/' . $uri3 : '');
+		
+		$queryString = $this->buildQueryString($params, $qs, $value);
+		
+		$url .= ($queryString ? '?' . $queryString : '') ;
+		return $url;
+	}
+	
+	function buildQueryString($params, $qs = null, $value = null) {
+		
+		$queryString = '';
+		
+		if ($qs == 'pp') {
+			$queryString .= 'p=0';
+		} else {
+			if ($qs == 'p') {
+				$queryString .= 'p=' . $value;
+			} else {
+				$queryString .= 'p=' . $params['page'];
+			}
+		}
+		
+		if ($params['perPage'] ) {
+			if ($qs == 'pp') {
+				$queryString .= '&pp=' . $value;
+			} else {
+				$queryString .= '&pp=' . $params['perPage'];
+			}
+		} else {
+			$queryString .= '&pp=';
+		}
+		
+		if ($params['sort'] ) {
+			if ($qs == 'sort') {
+				$queryString .= '&sort=' . $value;
+			} else {
+				$queryString .= '&sort=' . $params['sort'];
+			}
+		} else {
+			$queryString .= '&sort=';
+		}
+		
+		if ($params['order'] ) {
+			if ($qs == 'order') {
+				$queryString .= '&order=' . $value;
+			} else {
+				$queryString .= '&order=' . $params['order'];
+			}
+		} else {
+			$queryString .= '&order=';
+		}
+		
+		if ($params['filter'] ) {
+			if ($qs == 'f') {
+				$queryString .= '&f=' . $value;
+			} else {
+				$queryString .= '&f=' . $params['filter'];
+			}
+		} else {
+			$queryString .= '&f=';
+		}
+		
+		if ($params['q'] ) {
+			$queryString .= '&q=' . $params['q'];
+		} else {
+			$queryString .= '&q=';
+		}
+		
+		if ($params['city'] ) {
+			$queryString .= '&city=' . $params['city'];
+		} else {
+			$queryString .= '&city=';
+		}
+		
+		return $queryString;
+	}
+	
 }
 
 
