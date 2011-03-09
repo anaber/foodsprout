f77f1432998b2d71e530496a033e1eae9e9eea93
rj_bautista_05 <rj.bautista.11@gmail.com>
2011-03-09 10:28:26 +0000

    add functions for add, edit

--- application/controllers/admincp/country.php
+++ application/controllers/admincp/country.php
@@ -19,6 +19,46 @@ class Country extends Controller {
 		$this->list_country();
 	}
 	
+	// Search for a country
+	function ajaxSearchCountry() {
+		$this->load->model('CountryModel', '', TRUE);
+		$countries = $this->CountryModel->getCountryJsonAdmin();
+		echo json_encode($countries);
+	}
+
+	
+	function add() {
+		$data = array();
+		
+		// List of views to be included
+		$data['CENTER'] = array(
+				'form' => 'admincp/country_form',
+			);
+		
+		// Data to be passed to the views
+		$data['data']['center']['form']['VIEW_HEADER'] = "Add Country";
+		
+		$this->load->view('admincp/templates/center_template', $data);
+	}
+	
+	function update($id) {
+		$data = array();
+		
+		$this->load->model('CountryModel');
+		$country = $this->CountryModel->getCountryFromId($id);
+		
+		// List of views to be included
+		$data['CENTER'] = array(
+				'form' => 'admincp/country_form',
+			);
+		
+		// Data to be passed to the views
+		$data['data']['center']['form']['VIEW_HEADER'] = "Update City";
+		$data['data']['center']['form']['COUNTRY'] = $country;
+		
+		$this->load->view('admincp/templates/center_template', $data);
+	}	
+	
 	// List all the country in the database
 	function list_country()
 	{
@@ -42,30 +82,43 @@ class Country extends Controller {
 	}
 	
 	// Add a country to the database
-	function add_country()
+	function save_add()
 	{
 		// field name, error message, validation rules
 		$this->load->library('form_validation');
 		
-		$this->form_validation->set_rules('country_name', 'Fish Name', 'trim|required');
-		if($this->form_validation->run() == FALSE)
-		{
-			$this->index();
-		}
-		else
-		{
+		$this->form_validation->set_rules('country_name', 'Country Name', 'trim|required');
+
+		if($this->form_validation->run() == FALSE) {
+			echo validation_errors();
+		}else{
 			$this->load->model('CountryModel');
+
 			if($query = $this->CountryModel->addCountry())
-			{
-				redirect('admincp/country');
-			}
-			else
-			{
-				$this->index();
-			}
+				echo 'yes';
 		}
 		
 	}
+
+	// Update country
+	function save_edit()
+	{
+		// field name, error message, validation rules
+		$this->load->library('form_validation');
+		
+		$this->form_validation->set_rules('country_name', 'Country Name', 'trim|required');
+
+		if($this->form_validation->run() == FALSE) {
+			echo validation_errors();
+		}else{
+			$this->load->model('CountryModel');
+
+			if($query = $this->CountryModel->updateCountry())
+				echo 'yes';
+		}
+		
+	}
+
 }
 
 /* End of file country.php */
