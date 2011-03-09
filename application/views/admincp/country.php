36494e4762691786f9983de7b1ce7013f0484d84
rj_bautista_05 <rj.bautista.11@gmail.com>
2011-03-09 10:27:11 +0000

    update country.php create country_form.php

--- application/views/admincp/country.php
+++ application/views/admincp/country.php
@@ -1,47 +1,7 @@
-<?php
-if (count($COUNTRIES) > 0 ) {
-?>
-<table cellpadding="3" cellspacing="0" border="0" id="tbllist">
-	<tr>
-		<th>Country Id</th>
-		<th>Country Name</th>
-	</tr>
-			
-	
-<?php
+<?php echo anchor('admincp/country/add', 'Add Country'); ?><br /><br />
 
-	$i = 0;
-	foreach($COUNTRIES as $r) :
-		$i++;
-		echo '<tr class="d'.($i & 1).'">';
-		echo '	<td>'.$r->countryId.'</td>';
-		echo '	<td>'.$r->countryName.'</td>';
-		echo '</tr>';
- 	endforeach;
-?>
+<script src="<?php echo base_url()?>js/jquery.colorize.js" type="text/javascript"></script>
+<script src="<?php echo base_url()?>js/admin/country_search_admin.js" type="text/javascript"></script>
+<script src="<?php echo base_url()?>js/admin/admin_ajax_page_common_content.js" type="text/javascript"></script>
 
-<?php
-} else {
-	echo "No country available";
-}
-
-?>
-</table>
-
-
-
-<hr size="1">
-<b>Add Country</b><br>
-
-<?php
-	echo '<table cellpadding="2" style="-moz-border-radius: 4px;
-	-webkit-border-radius: 4px;
-	background: #EEEEEE;
-	color: #000000;
-	-moz-box-shadow: 0 1px 0 #CCCCCC;
-	-webkit-box-shadow: 0 1px 0 #CCCCCC;padding:10px; width:330px;"><tr><td>';
-	echo form_open('admincp/country/add_country');
-	echo 'Country Name:'. form_input('country_name', '').'<br><br>';
-	echo form_submit('submit', 'Add Country');
-	echo '</form></td></tr></table>';
-?>
+<?php $this->load->view('admincp/includes/ajax_paging'); ?>
