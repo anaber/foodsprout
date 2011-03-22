130f1c8e5bb4e22ced8dc8e73a19d392d0dc4b56
onemediainc <anaber@gmail.com>
2011-03-22 04:55:37 +0000

    More updates for Vanilla

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -8,7 +8,7 @@
 
 <!-- footer -->
 <div id="footerbg">
-  <div id="footertext"><?php echo anchor('about', 'About'); ?> | <?php echo anchor('about/contact', 'Contact'); ?> | <?php echo anchor('http://blog.foodsprout.com', 'Blog'); ?> | <?php echo anchor('about/business', 'Businesses'); ?> | <?php echo anchor('about/terms', 'Terms'); ?> | <?php echo anchor('about/privacy', 'Privacy'); ?></div>
+  <div id="footertext"><?php echo anchor('about', 'About'); ?> | <?php echo anchor('about/contact', 'Contact'); ?> | <?php echo anchor('/topics/', 'Topics'); ?> | <?php echo anchor('http://blog.foodsprout.com', 'Blog'); ?> | <?php echo anchor('about/business', 'Businesses'); ?> | <?php echo anchor('about/terms', 'Terms'); ?> | <?php echo anchor('about/privacy', 'Privacy'); ?></div>
 </div>
 <br /><br />
 <!-- end footer -->
