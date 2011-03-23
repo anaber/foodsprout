19edb9d78d982e47ffd9256a3f9465a8599c1b34
rj_bautista_05 <rj.bautista.11@gmail.com>
2011-03-23 16:48:34 +0000

    change redirect var to frm

--- application/views/includes/menu_login.php
+++ application/views/includes/menu_login.php
@@ -1,5 +1,5 @@
 <span id="signup">
 	<div id="membername">
-		<?php echo anchor('login?redirect='.$_SERVER['REQUEST_URI'], 'Sign In', 'style="font-size:13px;text-decoration:none;"'); ?> | <?php echo anchor('/login?redirect='.$_SERVER['REQUEST_URI'], 'Create Account', 'style="font-size:13px;text-decoration:none;"'); ?>
+		<?php echo anchor('login?frm='.$_SERVER['REQUEST_URI'], 'Sign In', 'style="font-size:13px;text-decoration:none;"'); ?> | <?php echo anchor('/login?frm='.$_SERVER['REQUEST_URI'], 'Create Account', 'style="font-size:13px;text-decoration:none;"'); ?>
 	</div>
 </span>
