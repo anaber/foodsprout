f40b5da626cddfdec032293a54d93f1df1e59ef1
onemediainc <onemediainc@53cad62b-6926-41e7-91f1-72a61ceabebc>
2011-03-29 06:37:14 +0000

    Updates to the cities listing for farmers market

--- trunk/application/views/includes/breadcrumb.php
+++ trunk/application/views/includes/breadcrumb.php
@@ -5,13 +5,13 @@ foreach($BREADCRUMB as $text => $link) {
 	
 	if ($i > 0) {
 		if ( !empty( $link) ) {
-			echo ' / ' . '<a href = "' . $link . '" class="redtxt">' . $text . '</a>';
+			echo ' / ' . '<a href = "' . $link . '">' . $text . '</a>';
 		} else {
 			echo ' / ' . '<h1>'.$text.'</h1>';
 		}
 	} else {
 		if ( !empty( $link) ) {
-			echo '<a href = "' . $link . '" class="redtxt">' . $text . '</a>';
+			echo '<a href = "' . $link . '">' . $text . '</a>';
 		} else {
 			echo '<h1>'.$text.'</h1>';
 		}
