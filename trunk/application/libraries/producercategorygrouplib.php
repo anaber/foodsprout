e4e40886f28ce7e5a6aa4a47386fb34e4641f40a
rj_bautista_05 <rj_bautista_05@53cad62b-6926-41e7-91f1-72a61ceabebc>
2011-03-29 18:54:04 +0000

    Fix the producer category in the admincp

---
+++ trunk/application/libraries/producercategorygrouplib.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class ProducerCategoryGroupLib {
+	
+	var $producerCategoryGroupId;
+	var $producerCategoryGroup;
+
+	function ProducerCategoryGroupLib() {
+		$this->producerCategoryGroupId = '';
+		$this->producerCategoryGroup = '';
+	}
+}
+?>
