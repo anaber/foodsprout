aca9ae0215782c408eb857111d6bc97b93161811
deepak2305 <deepak@yuvisha.com>
2011-04-29 07:18:05 +0000

    Incomplete business account pages

---
+++ application/views/business/register/step2.php
@@ -0,0 +1,56 @@
+<script>
+	$(document).ready(function() {
+		$("#frmAccount").validationEngine({
+			scroll:false,
+			unbindEngine:false
+		});
+	});
+</script>
+
+<?php
+	$this->load->view('business/register/top_steps');
+?>
+
+<div class = "content">
+
+	<div style = "border:1px dashed; padding:15px;margin-top:15px;" align = "center">
+		<div style = "font-size:22px;">We need to call you to verify your account ownership.</div>
+		<div class = "clear"></div>
+		<br />
+		<div style = "font-size:18px;">We are calling you to (425) 123-4657</div>
+	</div>
+	<div class = "clear"></div>
+	<br />
+	<div class = "flt"><strong>Frequently asked questions?</strong></div>
+	<div class = "clear"></div>
+	
+	<div style = "padding-left:20px;width:450px;" class = "flt">
+		<ul>
+			<li>Lorem Ipsum is simply dummy text.</li>
+			<li>Lorem Ipsum is simply dummy text.</li>
+			<li>Lorem Ipsum is simply dummy text.</li>
+			<li>Lorem Ipsum is simply dummy text.</li>
+		</ul>
+	</div>
+	<div style = "padding-left:20px;width:450px;" class = "flt">
+		<ul>
+			<li>Lorem Ipsum is simply dummy text.</li>
+			<li>Lorem Ipsum is simply dummy text.</li>
+			<li>Lorem Ipsum is simply dummy text.</li>
+			<li>Lorem Ipsum is simply dummy text.</li>
+		</ul>
+	</div>
+	<div class = "clear"></div>
+	
+	<div style = "padding:15px;margin-top:15px;" align = "center">
+		<div style = "font-size:22px;">Enter this code: 5345345</div>
+		<div class = "clear"></div>
+		<div id="signup-form1">
+			<input type="submit" name="submit" value="Call me now">
+		</div>
+		
+	</div>
+	<div class = "clear"></div>
+	
+</div>
+<div class = "clear"></div>
