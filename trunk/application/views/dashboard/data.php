6879a4cf1d4df2d2565957bf0a03e9c680d13d96
onemediainc <onemediainc@53cad62b-6926-41e7-91f1-72a61ceabebc>
2011-05-24 09:28:01 +0000

    Dashboard  updates, css fix

---
+++ trunk/application/views/dashboard/data.php
@@ -0,0 +1,3 @@
+Add Data<br><br>
+
+<a href="/dashboard/addrestaurant">Restaurant icon (250x250)</a> <a href="/dashboard/addfarm">Farm icon (250x250)</a> <a href="/dashboard/addmarket">Market icon (250x250)</a> 
--- trunk/application/views/includes/menu_logout.php
+++ trunk/application/views/includes/menu_logout.php
@@ -1,10 +1,11 @@
 <?php
 	$method = $this->uri->segment(2);
+	$dash = $this->uri->segment(1);
 ?>
 <span id="signup">
 	<div id="membername">
 		<strong><?php echo $this->session->userdata('firstName'); ?></strong> | 
-		<?php echo anchor('user/dashboard', 'Dashboard', 'style="font-size:13px;text-decoration:none;"'); ?> | 
+		<?php echo ( !empty($dash) &&  ( $dash == 'dashboard' ) ) ? '<strong>Dashboard</strong>' : anchor('/dashboard', 'Dashboard', 'style="font-size:13px;text-decoration:none;"'); ?> | 
 		<?php echo ( !empty($method) &&  ( $method == 'settings' || $method == 'password' ) ) ? '<strong>Settings</strong>' : anchor('user/settings', 'Settings', 'style="font-size:13px;text-decoration:none;"'); ?> | 
 		<?php 
 		if ($this->session->userdata('isAuthenticated') == 1 && $this->session->userdata('access') == 'admin' ) {
