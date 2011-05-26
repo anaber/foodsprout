098db2f1abf9cb5f4565ae3efca21c912db01b49
deepak2305 <deepak@yuvisha.com>
2011-05-26 01:12:41 +0000

    My Comment listing My Farm listing My Market listing My Menu listing My Supplier listing

--- application/models/commentmodel.php
+++ application/models/commentmodel.php
@@ -304,6 +304,146 @@ class CommentModel extends Model{
 		return $this->CommentLib;
 	}
 	
+	function getCommentsFromUserJson() {
+		global $PER_PAGE;
+		
+		$p = $this->input->post('p'); 
+		if (!$p) {
+			$p = $this->input->get('p');
+		}
+		$pp = $this->input->post('pp'); 
+		if (!$pp) {
+			$pp = $this->input->get('pp');
+		}
+		$sort = $this->input->post('sort');
+		if (!$sort) {
+			$sort = $this->input->get('sort');
+		}
+		$order = $this->input->post('order');
+		if (!$order) {
+			$order = $this->input->get('order');
+		}
+
+		$q = $this->input->post('q'); 
+		if (!$q) {
+			$q = $this->input->get('q');
+		}
+		//$q = '174538';
+		if ($q == '0') {
+			$q = '';
+		}
+		
+		$CI =& get_instance();
+		
+		//$CI->load->model('RestaurantModel');
+		//$restaurant = $CI->RestaurantModel->getRestaurantFromId($q);
+		//$restaurantChainId = $restaurant->restaurantChainId;
+		
+		$start = 0;
+		$page = 0;
+		
+		$base_query = 'SELECT comment.*, user.email, user.first_name' .
+				' FROM comment, user';
+		
+		$base_query_count = 'SELECT count(*) AS num_records' .
+				' FROM comment, user';
+				
+		$where = ' WHERE ';
+		$where .= ' comment.user_id  = ' . $q;
+		
+		$where .= ' AND comment.status = \'live\'' .
+				' AND comment.user_id = user.user_id ';
+		
+		$base_query_count = $base_query_count . $where;
+		
+		$query = $base_query_count;
+		
+		$result = $this->db->query($query);
+		$row = $result->row();
+		$numResults = $row->num_records;
+		
+		$query = $base_query . $where;
+		
+		if ( empty($sort) ) {
+			$sort_query = ' ORDER BY added_on';
+			$sort = 'added_on';
+		} else {
+			$sort_query = ' ORDER BY ' . $sort;
+		}
+		
+		if ( empty($order) ) {
+			$order = 'DESC';
+		}
+		
+		$query = $query . ' ' . $sort_query . ' ' . $order;
+		
+		if (!empty($pp) && $pp != 'all' ) {
+			$PER_PAGE = $pp;
+		}
+		
+		if (!empty($pp) && $pp == 'all') {
+			// NO NEED TO LIMIT THE CONTENT
+		} else {
+			
+			if (!empty($p) || $p != 0) {
+				$page = $p;
+				$p = $p * $PER_PAGE;
+				$query .= " LIMIT $p, " . $PER_PAGE;
+				$start = $p;
+				
+			} else {
+				$query .= " LIMIT 0, " . $PER_PAGE;
+			}
+		}
+		
+		log_message('debug', "CommentModel.getCommentsJson : " . $query);
+		$result = $this->db->query($query);
+		
+		$menu = array();
+		
+		foreach ($result->result_array() as $row) {
+			
+			$this->load->library('CommentLib');
+			unset($this->CommentLib);
+			
+			$this->CommentLib->commentId = $row['comment_id'];
+			$this->CommentLib->comment = nl2br($row['comment']);
+			$this->CommentLib->userId = $row['user_id'];
+			
+			$firstName = $row['first_name'];
+			$arrFirstName = explode(' ', $firstName);
+			$this->CommentLib->firstName = $arrFirstName[0];
+			
+			$this->CommentLib->email = $row['email'];
+			$this->CommentLib->ip = $row['track_ip'];
+			$this->CommentLib->status = $row['status'];
+			$this->CommentLib->addedOn = date('Y M, d H:i:s', strtotime ($row['added_on'] ) ) ;
+			
+			$menu[] = $this->CommentLib;
+			unset($this->CommentLib);
+		}
+		
+		if (!empty($pp) && $pp == 'all') {
+			$PER_PAGE = $numResults;
+		}
+		
+		$totalPages = ceil($numResults/$PER_PAGE);
+		$first = 0;
+		if ($totalPages > 0) {
+			$last = $totalPages - 1;
+		} else {
+			$last = 0;
+		}
+		
+		$params = requestToParams($numResults, $start, $totalPages, $first, $last, $page, $sort, $order, $q, '', '');
+		$arr = array(
+			'results'    => $menu,
+			'param'      => $params,
+	    );
+	    
+	    return $arr;
+	}
+	
 }
 
 
--- application/views/dashboard/farms.php
+++ application/views/dashboard/farms.php
@@ -1,1 +1,68 @@
-Added Farms on dashboard
+<script src="<?php echo base_url()?>js/dashboard/common.js" type="text/javascript"></script>
+<script src="<?php echo base_url()?>js/dashboard/farm.js" type="text/javascript"></script>
+
+<script>
+	
+	var userId = <?php echo $this->session->userdata('userId'); ?>;
+	
+	var jsonData;
+	var currentContent;
+	
+	var toggleDuration = 1000;
+	var isSupplierFormVisible = false;
+	var isMenuFormVisible = false;
+	var isCommentFormVisible = false;
+	
+	$(document).ready(function() {
+		
+		$.post("/user/ajaxFarmsByUser", { q: userId },
+		function(data){
+			currentContent = 'farms';
+			jsonData = data;
+			redrawContent(data, 'farms');
+		},
+		"json");
+	});
+	
+</script>
+
+<div id="alert"></div>
+<!-- center tabs -->
+
+	<div>
+		<div style="float:left;width:530px;"><h1>My Farms</h1></div>
+		<div class="clear"></div>
+		<hr size="1">
+		<div class="clear"></div>
+	</div>
+	<div class = "clear"></div>
+	
+	<div id="resultsContainer" style = "border-style:solid;border-width:0px;border-color:#FF0000;">
+		
+		<div id="resultTableContainer" class="menus" style = "width:790px; padding:0px;"></div>
+		<div class = "clear"></div>
+		
+		<div style="overflow:auto; padding:5px;">
+			<div style="float:left; width:110px; font-size:10px;border-style:solid;border-width:0px;border-color:#FF0000;" id = 'numRecords'>Records 0-0 of 0</div>
+			
+			<div style="float:left; width:500px; font-size:10px;border-style:solid;border-width:0px;border-color:#FF0000;" id = 'pagingLinks' align = "center">
+				<a href="#" id = "imgFirst">First</a> &nbsp;&nbsp;
+				<a href="#" id = "imgPrevious">Previous</a>
+				&nbsp;&nbsp;&nbsp; Page 1 of 1 &nbsp;&nbsp;&nbsp;
+				<a href="#" id = "imgNext">Next</a> &nbsp;&nbsp;
+				<a href="#" id = "imgLast">Last</a>
+			</div>
+			
+			<div style="float:right; width:165px; font-size:10px;border-style:solid;border-width:0px;border-color:#FF0000;" id = 'recordsPerPage' align = "right">
+				Items per page:&nbsp;
+				<a href="#" id = "10PerPage">10</a> | 
+				<a href="#" id = "20PerPage">20</a> |  
+				<a href="#" id = "40PerPage">40</a> |  
+				<a href="#" id = "50PerPage">50</a> 
+			</div>
+			
+			<div class="clear"></div>
+		</div>
+		
+	</div>
+<!-- end center tabs -->
