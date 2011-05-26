7427d10cb03b4003e90716bc7e6ded5e2d42dcca
deepak2305 <deepak@yuvisha.com>
2011-05-26 17:23:55 +0000

    Update CSS on dashboard

--- application/views/dashboard/data.php
+++ application/views/dashboard/data.php
@@ -1,7 +1,33 @@
-Add Data<br><br>
-
-<a href="/dashboard/addrestaurant"><img src="/img/icons/restaurant.png" border="0" width="150" height="150"></a><span style="font-size: 30px;"><a href="/dashboard/addrestaurant">+ Restaurant</a><br/>
-
-<br/><a href="/dashboard/addfarm"><img src="/img/icons/farm.png" border="0" width="150" height="150"></a> <span style="font-size: 30px;"><a href="/dashboard/addfarm">+ Farm</a><br/>
-
-<a href="/dashboard/addmarket"><img src="/img/icons/farm_market.png" border="0" width="150" height="150"></a> <span style="font-size: 30px;"><a href="/dashboard/addmarket">+ Farmer's Market</a><br/>
+	<div>
+		<div style="float:left;width:530px;"><h1>Add Data</h1></div>
+		<div class="clear"></div>
+		<hr size="1">
+		<div class="clear"></div>
+	</div>
+	<div class = "clear"></div>
+	
+	<div style = "float:left;">
+		<a href="/dashboard/addrestaurant"><img src="/img/icons/restaurant.png" border="0" width="150" height="150"></a>
+	</div>
+	<div style = "float:left;margin-top:50px;padding-left:20px;">
+		<span style="font-size: 30px;"><a href="/dashboard/addrestaurant">+ Restaurant</a>
+	</div>
+	<div class = "clear"></div>
+	<br />
+	
+	<div style = "float:left;">
+		<a href="/dashboard/addfarm"><img src="/img/icons/farm.png" border="0" width="150" height="150"></a>
+	</div>
+	<div style = "float:left;margin-top:50px;padding-left:20px;">
+		<span style="font-size: 30px;"><a href="/dashboard/addfarm">+ Farm</a>
+	</div>
+	<div class = "clear"></div>
+	<br />
+	
+	<div style = "float:left;">
+		<a href="/dashboard/addmarket"><img src="/img/icons/farm_market.png" border="0" width="150" height="150"></a>
+	</div>
+	<div style = "float:left;margin-top:50px;padding-left:20px;">
+		<span style="font-size: 30px;"><a href="/dashboard/addmarket">+ Farmer's Market</a>
+	</div>
+	<div class = "clear"></div>
--- application/views/dashboard/managedata.php
+++ application/views/dashboard/managedata.php
@@ -1,7 +1,33 @@
-
-
-<a href="/dashboard/addrestaurant"><img src="/img/icons/restaurant.png" border="0" width="150" height="150"></a><span style="font-size: 30px;"><a href="/dashboard/addrestaurant">View Restaurant</a><br/>
-
-<br/><a href="/dashboard/addfarm"><img src="/img/icons/farm.png" border="0" width="150" height="150"></a> <span style="font-size: 30px;"><a href="/dashboard/addfarm">View Farm</a><br/>
-
-<a href="/dashboard/addmarket"><img src="/img/icons/farm_market.png" border="0" width="150" height="150"></a> <span style="font-size: 30px;"><a href="/dashboard/addmarket">View Farmer's Market</a><br/>
+	<div>
+		<div style="float:left;width:530px;"><h1>Manage Data</h1></div>
+		<div class="clear"></div>
+		<hr size="1">
+		<div class="clear"></div>
+	</div>
+	<div class = "clear"></div>
+	
+	<div style = "float:left;">
+		<a href="/dashboard/restaurants"><img src="/img/icons/restaurant.png" border="0" width="150" height="150"></a>
+	</div>
+	<div style = "float:left;margin-top:50px;padding-left:20px;">
+		<span style="font-size: 30px;"><a href="/dashboard/restaurants">View Restaurant</a>
+	</div>
+	<div class = "clear"></div>
+	<br />
+	
+	<div style = "float:left;">
+		<a href="/dashboard/farms"><img src="/img/icons/farm.png" border="0" width="150" height="150"></a>
+	</div>
+	<div style = "float:left;margin-top:50px;padding-left:20px;">
+		<span style="font-size: 30px;"><a href="/dashboard/farms">View Farm</a>
+	</div>
+	<div class = "clear"></div>
+	<br />
+	
+	<div style = "float:left;">
+		<a href="/dashboard/markets"><img src="/img/icons/farm_market.png" border="0" width="150" height="150"></a>
+	</div>
+	<div style = "float:left;margin-top:50px;padding-left:20px;">
+		<span style="font-size: 30px;"><a href="/dashboard/markets">View Farmer's Market</a>
+	</div>
+	<div class = "clear"></div>
